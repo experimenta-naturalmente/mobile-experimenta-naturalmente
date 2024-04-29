3e275e40d63ee629c6e8ad57efbf34a29a2fc512
Caio Araújo Batista <caio.araujo@edu.pucrs.br>
2024-04-29 17:31:41 +0000

    fix(us-01): Remove WeekTimeComponent

--- lib/features/signup/presentation/screens/signup_page_working_hours.dart
+++ lib/features/signup/presentation/screens/signup_page_working_hours.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:turismo_rural_frontend/features/signup/presentation/widgets/week_time_component.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:turismo_rural_frontend/features/signup/bloc/signup_bloc/signup_bloc.dart';
+import 'package:turismo_rural_frontend/features/signup/bloc/signup_bloc/signup_state.dart';
+import 'package:turismo_rural_frontend/features/signup/presentation/widgets/shift_select.dart';
 
 class SignUpPageWorkingHours extends StatelessWidget {
   const SignUpPageWorkingHours({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final signUpBloc = BlocProvider.of<SignUpBloc>(context);
+    final checkboxNotifier =
+        (signUpBloc.state as SignUpPageWorkingHoursState).checkboxNotifier;
+
+    const segundaFeira = SignUpPageWorkingHoursState.segundaFeira;
+    const tercaFeira = SignUpPageWorkingHoursState.tercaFeira;
+    const quartaFeira = SignUpPageWorkingHoursState.quartaFeira;
+    const quintaFeira = SignUpPageWorkingHoursState.quintaFeira;
+    const sextaFeira = SignUpPageWorkingHoursState.sextaFeira;
+    const sabado = SignUpPageWorkingHoursState.sabado;
+    const domingo = SignUpPageWorkingHoursState.domingo;
+    const feriados = SignUpPageWorkingHoursState.feriados;
+
     final screenWidth = MediaQuery.of(context).size.width;
     return Center(
       child: SingleChildScrollView(
@@ -24,42 +40,222 @@ class SignUpPageWorkingHours extends StatelessWidget {
               Container(
                 alignment: Alignment.center,
                 decoration: BoxDecoration(
-                  // substituir isso por surface color, container color ou coisa do tipo, em colorScheme.
                   color: Theme.of(context).brightness == Brightness.light
-                      ? Colors.white
-                      : Colors.black,
+                      ? Theme.of(context).colorScheme.surface
+                      : Theme.of(context).colorScheme.onSurface,
                   borderRadius: BorderRadius.circular(20),
                 ),
                 padding: const EdgeInsets.only(bottom: 3),
-                // ListView é componente pra carregar lista dinâmica lazily e com scroll.
-                // Essa lista nao tem necessidade de nenhum dos dois. Substituir por Column
-                child: const Column(
+                child: ListView(
+                  shrinkWrap: true,
                   children: [
-                    // "WeekTimeComponent" não precisa existir. Ver: https://m3.material.io/components/checkbox/overview
-                    //  tudo é só uma checkbox com um texto, nao justifica uma widget pra isso
-                    WeekTimeComponent(
-                      text: 'Segunda-feira',
+                    ValueListenableBuilder<bool>(
+                      valueListenable: checkboxNotifier[segundaFeira]!,
+                      builder: (context, value, child) {
+                        return CheckboxListTile(
+                          title: const Text(segundaFeira),
+                          value: checkboxNotifier[segundaFeira]!.value,
+                          onChanged: (bool? newValue) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Dialog(
+                                  child: SizedBox(
+                                    width: 300,
+                                    height: 600,
+                                    child: ShiftSelect(
+                                      checkboxNotifier:
+                                          checkboxNotifier[segundaFeira]!,
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        );
+                      },
                     ),
-                    WeekTimeComponent(
-                      text: 'Terça-feira',
+                    ValueListenableBuilder<bool>(
+                      valueListenable: checkboxNotifier[tercaFeira]!,
+                      builder: (context, value, child) {
+                        return CheckboxListTile(
+                          title: const Text(tercaFeira),
+                          value: checkboxNotifier[tercaFeira]!.value,
+                          onChanged: (bool? newValue) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Dialog(
+                                  child: SizedBox(
+                                    width: 300,
+                                    height: 600,
+                                    child: ShiftSelect(
+                                      checkboxNotifier:
+                                          checkboxNotifier[tercaFeira]!,
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        );
+                      },
                     ),
-                    WeekTimeComponent(
-                      text: 'Quarta-feira',
+                    ValueListenableBuilder<bool>(
+                      valueListenable: checkboxNotifier[quartaFeira]!,
+                      builder: (context, value, child) {
+                        return CheckboxListTile(
+                          title: const Text(quartaFeira),
+                          value: checkboxNotifier[quartaFeira]!.value,
+                          onChanged: (bool? newValue) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Dialog(
+                                  child: SizedBox(
+                                    width: 300,
+                                    height: 600,
+                                    child: ShiftSelect(
+                                      checkboxNotifier:
+                                          checkboxNotifier[quartaFeira]!,
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        );
+                      },
                     ),
-                    WeekTimeComponent(
-                      text: 'Quinta-feira',
+                    ValueListenableBuilder<bool>(
+                      valueListenable: checkboxNotifier[quintaFeira]!,
+                      builder: (context, value, child) {
+                        return CheckboxListTile(
+                          title: const Text(quintaFeira),
+                          value: checkboxNotifier[quintaFeira]!.value,
+                          onChanged: (bool? newValue) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Dialog(
+                                  child: SizedBox(
+                                    width: 300,
+                                    height: 600,
+                                    child: ShiftSelect(
+                                      checkboxNotifier:
+                                          checkboxNotifier[quintaFeira]!,
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        );
+                      },
                     ),
-                    WeekTimeComponent(
-                      text: 'Sexta-feira',
+                    ValueListenableBuilder<bool>(
+                      valueListenable: checkboxNotifier[sextaFeira]!,
+                      builder: (context, value, child) {
+                        return CheckboxListTile(
+                          title: const Text(sextaFeira),
+                          value: checkboxNotifier[sextaFeira]!.value,
+                          onChanged: (bool? newValue) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Dialog(
+                                  child: SizedBox(
+                                    width: 300,
+                                    height: 600,
+                                    child: ShiftSelect(
+                                      checkboxNotifier:
+                                          checkboxNotifier[sextaFeira]!,
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        );
+                      },
                     ),
-                    WeekTimeComponent(
-                      text: 'Sábado',
+                    ValueListenableBuilder<bool>(
+                      valueListenable: checkboxNotifier[sabado]!,
+                      builder: (context, value, child) {
+                        return CheckboxListTile(
+                          title: const Text(sabado),
+                          value: checkboxNotifier[sabado]!.value,
+                          onChanged: (bool? newValue) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Dialog(
+                                  child: SizedBox(
+                                    width: 300,
+                                    height: 600,
+                                    child: ShiftSelect(
+                                      checkboxNotifier:
+                                          checkboxNotifier[sabado]!,
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        );
+                      },
                     ),
-                    WeekTimeComponent(
-                      text: 'Domingo',
+                    ValueListenableBuilder<bool>(
+                      valueListenable: checkboxNotifier[domingo]!,
+                      builder: (context, value, child) {
+                        return CheckboxListTile(
+                          title: const Text(domingo),
+                          value: checkboxNotifier[domingo]!.value,
+                          onChanged: (bool? newValue) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Dialog(
+                                  child: SizedBox(
+                                    width: 300,
+                                    height: 600,
+                                    child: ShiftSelect(
+                                      checkboxNotifier:
+                                          checkboxNotifier[domingo]!,
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        );
+                      },
                     ),
-                    WeekTimeComponent(
-                      text: 'Feriados',
+                    ValueListenableBuilder<bool>(
+                      valueListenable: checkboxNotifier[feriados]!,
+                      builder: (context, value, child) {
+                        return CheckboxListTile(
+                          title: const Text(feriados),
+                          value: checkboxNotifier[feriados]!.value,
+                          onChanged: (bool? newValue) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Dialog(
+                                  child: SizedBox(
+                                    width: 300,
+                                    height: 600,
+                                    child: ShiftSelect(
+                                      checkboxNotifier:
+                                          checkboxNotifier[feriados]!,
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        );
+                      },
                     ),
                   ],
                 ),
