54bde0c159cae66f91a0f86c9ec18a9a58964c57
Vitor Kepeler Do Amaral <vitorkepeler@gmail.com>
2024-07-03 17:05:59 +0000

    Fix/event hours

--- lib/features/experiences/presentation/widgets/event_details.dart
+++ lib/features/experiences/presentation/widgets/event_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttericon/font_awesome_icons.dart';
+import 'package:intl/intl.dart';
 import 'package:turismo_rural_frontend/core/data/models/event.dart';
 
 class EventDetails extends StatelessWidget {
@@ -32,7 +33,12 @@ class EventDetails extends StatelessWidget {
               ),
               const SizedBox(height: 8),
               Text(
-                event.eventStart,
+                "Início: ${DateFormat('dd/MM/yyyy HH:mm').format(DateTime.parse(event.eventStart))}",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                "Fim: ${DateFormat('dd/MM/yyyy HH:mm').format(DateTime.parse(event.eventEnd))}",
                 style: Theme.of(context).textTheme.titleMedium,
               ),
               const SizedBox(height: 8),
